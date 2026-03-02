2167d0088fe50962e71070c41e8ae8b582cbb72c
BugSpace <409478@niuitmo.ru>
2026-03-02 22:26:39 +0700

    add classification of numbers

--- task_1.py
+++ task_1.py
@@ -10,5 +10,28 @@ def get_divisors_sum(n):
 
     return sum
 
-n = int(input("Введите n: "))
-print(get_divisors_sum(n))
+def classify(n, sum):
+    # Классифицирует число, исходя из суммы его делителей
+    categories = []
+
+    # Совершенные числа: s(n) = n
+    if sum == n:
+        categories.append("совершенное")
+
+    # Недостаточные числа: s(n) < n
+    if sum < n:
+        categories.append("недостаточное")
+
+    # Избыточные числа: s(n) > n
+    if sum > n:
+        categories.append("избыточное")
+
+    # Простые числа: s(n) = 1 (только для n > 1)
+    if sum == 1 and n > 1:
+        categories.append("простое")
+
+l = int(input("Введите левую границу в диапазоне: "))
+r = int(input("Введите правую границу в диапазоне: "))
+for i in range(l, r + 1):
+    s = get_divisors_sum(i)
+    print(f"Сумма собственных делителей числа {i}: {s}")
