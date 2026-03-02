a488257ed74f8938c3880e1660f9bd2b3e82120e
BugSpace <409478@niuitmo.ru>
2026-03-02 22:51:36 +0700

    add answer to the first question

--- task_2.py
+++ task_2.py
@@ -5,3 +5,17 @@ print(f"Введите {n} чисел:")
 for i in range(n):
     num = int(input(f"q_{i+1}: "))
     numbers.append(num)
+
+result_a = []
+result_b = []
+result_c = []
+
+# Удвоенные нечётные числа
+for num in numbers:
+        if num % 2 == 0 and (num // 2) % 2 != 0:
+            result_a.append(num)
+print("а) Удвоенные нечетные числа:")
+if result_a:
+    print(result_a)
+else:
+    print("Таких чисел нет")
