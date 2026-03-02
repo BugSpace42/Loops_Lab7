53f73e6ebdef2212ba31562e3587585cc6742591
BugSpace <409478@niuitmo.ru>
2026-03-02 23:11:15 +0700

    add solving

--- task_3.py
+++ task_3.py
@@ -4,3 +4,12 @@ print("Введите 10 действительных чисел:")
 for i in range(1, 11):
     num = float(input(f"a_{i}: "))
     numbers.append(num)
+
+total = 0
+for i in range(10):
+    power = i + 1
+    term = numbers[i] ** power
+    total += term
+    print(f"{numbers[i]}^{power} = {term}")
+
+print(f"Результат вычисления: {total}")
